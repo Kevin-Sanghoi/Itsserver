c1a9f08ee21a484bfce77988373e54ba8d2af16f
Kevin-Sanghoi <kevinsanghoi7@gmail.com>
2017-09-15 16:28:12 +0530

    again

--- itsserver/itsserver/settings.py
+++ itsserver/itsserver/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'itsserver.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'group17',
-        'USER': 'group17',
-        'PASSWORD': '17',
+        'NAME': 'itsdata',
+        'USER': 'kevin',
+        'PASSWORD': 'iiits@123',
         'HOST': 'localhost',
     }
 }
